94d80a7323b528edd3fa98ed3463604490118854
Thomas Van Laere <thomas.van.laere@atlascopco.com>
2024-12-03 16:25:06 +0100

    another small edit in Game

--- Game.py
+++ Game.py
@@ -2,7 +2,6 @@ import Users
 
 
 uname = input("Hi, what is your name? ")
-uemail = input("What is your email?")
-ubalance = input("Set your balance")
+ubalance = input("Set your balance: ")
 
-user = Users.Users(uname,uemail,ubalance)
+user = Users.Users(uname, ubalance)

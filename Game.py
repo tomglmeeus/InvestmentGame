ec643d10b05802f9714b8500621be0525e437082
ATLASCOPCO\AIR26945 <tom.meeus@atlascopco.com>
2024-12-03 16:14:07 +0100

    new files

---
+++ Game.py
@@ -0,0 +1,9 @@
+import Users
+
+
+uname = input("Hi, what is your name? ")
+uemail = input("What is your email?")
+ubalance = input("Set your balance")
+
+user = Users(uname,uemail,ubalance)
+
---
+++ Users.py
@@ -0,0 +1,5 @@
+class Users():
+    def __init__(self, username, email, balance):
+        self.username = username
+        self.email = email
+        self.balance = balance

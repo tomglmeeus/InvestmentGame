6331d1e9fafc1f309afcab328e9b9ca54cc13128
Bhagyashree Nannavre <bhagyashree.nannavre@atlascopco.com>
2024-12-03 16:31:46 +0100

    added few lines to Toms code

--- Game.py
+++ Game.py
@@ -5,5 +5,7 @@ uname = input("Hi, what is your name? ")
 uemail = input("What is your email?")
 ubalance = input("Set your balance")
 
-user = Users(uname,uemail,ubalance)
+user = Users.Users(uname,uemail,ubalance)
+print(f"hello ",user.username," your email id id",user.email," your account balanace is",user.balance)
+print("Do you want to buy stocks Y/N")
 

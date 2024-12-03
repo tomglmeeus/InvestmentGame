4c9e019efd74113d16a5f4ea82c1711b3571133b
Thomas Van Laere <thomas.van.laere@atlascopco.com>
2024-12-03 16:53:38 +0100

    made some edits, removing email and editing prompts

--- Game.py
+++ Game.py
@@ -2,11 +2,9 @@ import Users
 
 
 uname = input("Hi, what is your name? ")
-uemail = input("What is your email?")
-ubalance = input("Set your balance")
-
-user = Users.Users(uname, uemail,ubalance)
-print(f"hello ",user.username," your email id id",user.email," your account balanace is",user.balance)
-print("Do you want to buy stocks Y/N")
 ubalance = input("Set your balance: ")
+
 user = Users.Users(uname, ubalance)
+
+print(f"Hello {user.username} your account balanace is {user.balance}.")
+print("Do you want to buy stocks? (Y/N)")
